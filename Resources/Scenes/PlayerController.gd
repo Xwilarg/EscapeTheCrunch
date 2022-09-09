ac861a4584fe8e1de6b92f15681f2cf4efadf6f3
Xwilarg <xwilarg@protonmail.com>
2022-09-10 00:38:31 +0200

    Add ability to grab keys

--- Resources/Scenes/PlayerController.gd
+++ Resources/Scenes/PlayerController.gd
@@ -7,6 +7,9 @@ export var xSens = -1.0
 export var interactionDistance = 2
 
 var pressEUI: Object
+var safeTarget: Object
+var id = 1
+var currentKey: Object = null
 
 func _ready():
 	pressEUI = get_node("Label")
@@ -17,6 +20,11 @@ func _input(event):
 	if event is InputEventMouseMotion:
 		rotate_y(deg2rad(event.relative.x * xSens))
 		$Camera.rotate_x(deg2rad(event.relative.y * xSens))
+	if Input.is_action_pressed("action") and safeTarget != null and currentKey == null:
+		currentKey = safeTarget
+		safeTarget.hide()
+		pressEUI.hide()
+		safeTarget = null
 
 func _physics_process(delta):
 	# Controls
@@ -39,7 +47,9 @@ func _physics_process(delta):
 	var from = $Camera.project_ray_origin(center)
 	var to = from + $Camera.project_ray_normal(center) * 100
 	var result = get_world().direct_space_state.intersect_ray(from, to)
-	if result and result.collider.name == "Safe" and global_transform.origin.distance_to(result.collider.global_transform.origin) < interactionDistance:
+	if result and result.collider.name == "Safe" and global_transform.origin.distance_to(result.collider.global_transform.origin) < interactionDistance && result.collider.is_visible() and currentKey == null:
 		pressEUI.show()
+		safeTarget = result.collider
 	else:
 		pressEUI.hide()
+		safeTarget = null
