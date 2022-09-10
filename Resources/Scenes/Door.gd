a549eea63a771c29fc6513be0a1df7ab3bc249af
Xwilarg <xwilarg@protonmail.com>
2022-09-11 00:41:35 +0200

    Improve safe spawn

--- Resources/Scenes/Door.gd
+++ Resources/Scenes/Door.gd
@@ -4,6 +4,7 @@ var rng: RandomNumberGenerator
 var nodes: Array
 var indexes: Array
 var safePrefab: Object
+var instance: Object
 
 func _ready():
 	
@@ -12,13 +13,12 @@ func _ready():
 	nodes = get_node("../Spawns").get_children()
 	indexes = range(nodes.size())
 	safePrefab = load("res://Resources//Scenes/Safe.tscn")
+	instance = safePrefab.instance()
+	get_node("../../../..").add_child(instance);
 	respawn()
 
 func respawn():
 	var nb = rng.randi_range(0, indexes.size() - 1)
 	var value = indexes[nb]
-	var instance = safePrefab.instance()
-	instance.setId(nb)
-	get_node("../../../..").add_child(instance);
 	instance.translation = nodes[nb].translation
-	
+	instance.show()
