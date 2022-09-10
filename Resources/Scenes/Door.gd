b6962d9073e13a26ecff40a296f5d20049a54f27
Xwilarg <xwilarg@protonmail.com>
2022-09-11 00:28:48 +0200

    Change spawn

--- Resources/Scenes/Door.gd
+++ Resources/Scenes/Door.gd
@@ -1,10 +1,11 @@
 extends StaticBody
 
+var rng: RandomNumberGenerator
+var nodes: Array
+var indexes: Array
+var safePrefab: Object
+
 func _ready():
-	var rng: RandomNumberGenerator
-	var nodes: Array
-	var indexes: Array
-	var safePrefab: Object
 	
 	rng = RandomNumberGenerator.new()
 	rng.randomize()
@@ -12,10 +13,12 @@ func _ready():
 	indexes = range(nodes.size())
 	safePrefab = load("res://Resources//Scenes/Safe.tscn")
 
+
+func respawn():
 	var nb = rng.randi_range(0, indexes.size() - 1)
 	var value = indexes[nb]
 	var instance = safePrefab.instance()
 	instance.setId(nb)
+	get_node("../../../..").add_child(instance);
 	instance.translation = nodes[nb].translation
-	indexes.erase(nb)
-	nodes[nb].add_child(instance);
+	
