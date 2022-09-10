1e21e1d20bcfc493809be068e5a9f3f5c02a015f
Xwilarg <xwilarg@protonmail.com>
2022-09-10 16:32:54 +0200

    Fix rotation

--- Resources/Scenes/Boss.gd
+++ Resources/Scenes/Boss.gd
@@ -33,10 +33,8 @@ func advance():
 	if pos.distance_to(target_point) > 1:
 		# Calculate the velocity.
 		vel = (target_point - pos).slide(n).normalized() * speed;
-	var angle = Vector2(pos.x, pos.z).angle_to_point(Vector2(target_point.x, target_point.z));
-	print(angle);
-	self.set_rotation(Vector3(0, angle, 0));
 	move_and_slide(vel, Vector3(0, 1, 0));
+	look_at(transform.origin + vel, Vector3.UP)
 
 func _physics_process(delta):
 	if target.distance_to(self.translation) < 2.5:
