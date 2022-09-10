b186c33a78e494a48cbe8ac05e6a3978e2c44ab6
Xwilarg <xwilarg@protonmail.com>
2022-09-11 00:52:56 +0200

    Fix spawn

--- Resources/Scenes/Door.gd
+++ Resources/Scenes/Door.gd
@@ -4,21 +4,21 @@ var rng: RandomNumberGenerator
 var nodes: Array
 var indexes: Array
 var safePrefab: Object
-var instance: Object
+var currObj: Object
 
 func _ready():
-	
 	rng = RandomNumberGenerator.new()
 	rng.randomize()
 	nodes = get_node("../Spawns").get_children()
 	indexes = range(nodes.size())
 	safePrefab = load("res://Resources//Scenes/Safe.tscn")
-	instance = safePrefab.instance()
-	get_node("../../../..").add_child(instance);
+	currObj = safePrefab.instance()
+	var prevNode = get_node("../../../..")
+	prevNode.add_child(currObj);
 	respawn()
 
 func respawn():
 	var nb = rng.randi_range(0, indexes.size() - 1)
 	var value = indexes[nb]
-	instance.translation = nodes[nb].translation
-	instance.show()
+	currObj.translation = nodes[nb].translation
+	currObj.show()
